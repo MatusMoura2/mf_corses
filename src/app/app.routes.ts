6a94e20c3307accff9245838c98e909de653ade4
MatusMoura2 <matus.moura333x2@gmail.com>
2026-04-24 23:07:21 -0300

    concurso publico

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,11 +6,13 @@ import { Linguagens } from './components/linguagens/linguagens';
 
 import { LJava } from './components/l-java/l-java';
 import { LPython } from './components/l-python/l-python';
+import { ConcursoPublico } from './components/concurso-publico/concurso-publico';
 
 export const routes: Routes = [
   { path: 'dashboard', component: Dashboard },
   { path: 'linguagens', component: Linguagens },
   { path: 'l-java', component: LJava },
   { path: 'l-python', component: LPython },
+  { path: 'concursos', component: ConcursoPublico },
   { path: '', component: Home, pathMatch: 'full' }
 ];
---
+++ src/app/components/concurso-publico/concurso-publico.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-concurso-publico',
+  imports: [RouterLink],
+  templateUrl: './concurso-publico.html',
+  styleUrl: './concurso-publico.css',
+})
+export class ConcursoPublico {}
